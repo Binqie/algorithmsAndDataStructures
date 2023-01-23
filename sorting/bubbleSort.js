9f0a24705ff0d7e43241dacfe75e75a4f1320a9f
Binqie <shamir.beishebaev@icloud.com>
2023-01-23 20:37:21 +0600

    bubbleSort optimized

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,12 +1,24 @@
 function BubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
+        let isSorted = true;
+
         for (let j = 0; j < array.length - 1 - i; j++) {
             if (array[j] > array[j + 1]) {
+                isSorted = false;
                 let temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
             }
         }
+
+        if (isSorted) return array;
     }
     return array;
 }
+
+console.log(BubbleSort([0, 3, 4, -1, 5, 11]));
+console.log(BubbleSort([-0, -3, -4, -1, -5, -11]));
+console.log(BubbleSort([]));
+console.log(BubbleSort([4]));
+console.log(BubbleSort([0, 0, 0, 0, 0, 0]));
+console.log(BubbleSort([0, 0, 0, 0, 0, 0]));
